0926a9bd7ca0371585e093a9fcd60b0f39390e7c
Kateryna Chovhan <chovganek@gmail.com>
2021-10-10 21:32:51 +0300

    add work with contenteditable elements

--- src/content.js
+++ src/content.js
@@ -7,63 +7,95 @@ let dictionary = {
 };
 
 let dictKeys = Object.keys(dictionary);
-
-    document.addEventListener('keyup', event => {
-
-        if (event.code === 'Space') {
-            let elem = event.target;
-
-            if (elem.tagName.toLowerCase() == 'input' &&
-                elem.getAttribute('type').toLowerCase() == 'text') {
-                let elValue = elem.value.split(' ');
-                let lastInputWord = elValue[elValue.length - 2];
-
-                if (dictKeys.includes(lastInputWord)) {
-
-                    let replaceWordsArray = dictionary[lastInputWord];
-
-                    const body = document.querySelector('body');
-
-                    const popup = document.createElement('div');
-                    popup.classList.add('popup');
-                    const eclipse = document.createElement('div');
-                    eclipse.classList.add('eclipse');
-                    eclipse.append(popup);
-                    body.prepend(eclipse);
-                   
-                    const header = document.createElement('div');
-                    header.classList.add('header');
-                    const replaceWordsContainer = document.createElement('div');
-                    replaceWordsContainer.classList.add('replaceWordsContainer');
-                
-                    popup.append(header, replaceWordsContainer);
-                
-                    header.textContent = "How do you want to change:";
-                
-                    replaceWordsArray.forEach(element => {
-                        let replacementWord = document.createElement('div');
-                        replacementWord.classList.add('elementForReplace');
-                        replacementWord.textContent = element;
-                        replaceWordsContainer.append(replacementWord);
-                    });
-
-                    let targetWord;
-                    replaceWordsContainer.addEventListener('click', function(e){
-                        targetWord = e.target.innerText;
-                        elem.value = elem.value.replace(lastInputWord, targetWord);
-                        eclipse.remove();
-                        elem.focus();
-                    });
+let enteredText = '';
+let elementValue = '';
 
 
-                    //         if (request.todo == 'ChangeInputWord') {
-                    //             let replaceWord = request.clickedWord;
-                    //             event.target.value = elemValue.replace(lastInputWord, replaceWord);
-                    //             let focusInput = document.querySelector('.focusInput');
-                    //             focusInput.focus();
-                    //             event.target.classList.remove('focusInput')
-                    //         }
-                };
-            }
+const isTextEndsWithDictKey = (enteredText, dictKeys) => {
+    for (let key of dictKeys) {
+        if (enteredText.endsWith(key)) {
+            return key;
+        }
+    }
+    return false;
+}
+
+const replacementWord = (key) => {
+    let replaceWordsArray = dictionary[key];
+
+    const body = document.querySelector('body');
+
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    const eclipse = document.createElement('div');
+    eclipse.setAttribute('id', 'eclipse');
+    eclipse.classList.add('eclipse');
+    eclipse.append(popup);
+    body.prepend(eclipse);
+
+    const header = document.createElement('div');
+    header.classList.add('header');
+    const replaceWordsContainer = document.createElement('div');
+    replaceWordsContainer.classList.add('replaceWordsContainer');
+    replaceWordsContainer.setAttribute('id', 'replaceWordsContainer');
+
+    popup.append(header, replaceWordsContainer);
+
+    header.textContent = "How do you want to change:";
+
+    replaceWordsArray.forEach(element => {
+        let replacementWord = document.createElement('div');
+        replacementWord.classList.add('elementForReplace');
+        replacementWord.textContent = element;
+        replaceWordsContainer.append(replacementWord);
+    });
+
+}
+
+document.addEventListener('input', event => {
+    enteredText += event.data;
+     console.log(enteredText);
+
+    if (event.data === ' ') {
+        enteredText = enteredText.trimEnd();
+        let elem = event.target;
+        // console.log(event);
+
+        let key = isTextEndsWithDictKey(enteredText, dictKeys);
+        console.log(key)
+
+        if ((elem.tagName.toLowerCase() == 'input' &&
+                elem.getAttribute('type').toLowerCase() == 'text')) {
+            elementValue = elem.value;
+            console.log(elementValue);
+        }
+
+        if (elem.isContentEditable) {
+            elementValue = elem.textContent;
+            console.log(elementValue);
+        }
+
+        if (key) {
+            let textForReplace = enteredText.slice(0, -key.length);
+            console.log("entered text after slice: ", textForReplace)
+            replacementWord(key);
+
+            let targetWord;
+            replaceWordsContainer.addEventListener('click', function (e) {
+                targetWord = e.target.innerText;
+                textForReplace += targetWord + " ";
+                console.log('enteredText after replace: ', textForReplace)
+                elementValue = elementValue.replace(enteredText, textForReplace);
+                console.log(elementValue)
+                eclipse.remove();
+                elem.isContentEditable ? elem.textContent = elementValue :
+                    elem.value = elementValue;
+                    elem.focus();
+                enteredText = '';
+                elementValue = '';
+            });
+
         };
-    })
+
+    };
+})
